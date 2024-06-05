a5a97924c6bb732d0d188038248d0c3baa6506fd
Gustavo Almeida <159791843+gustavoalmeidapaula@users.noreply.github.com>
2024-06-05 16:04:51 -0300

    enter2

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,8 @@
-const button = document.getElementById('button')
-const main = document.querySelector('main')
-const theme = document.getElementById('themeSwitcher')
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleButton = document.getElementById('theme-toggle');
 
-theme.addEventListener('click', function () {
-    if (main.dataset.theme === 'dark') {
-        root.style.setProperty('$secundary-color', '#FFDFC0')
-        root.style.setProperty('$font-color', '#341f0a')
-        root.style.setProperty('$primary-color', '##ffffff')
-        main.dataset.theme = 'light'
-    } else {
-        root.style.setProperty('$secundary-color', '#ffffff')
-        root.style.setProperty('$font-color', '#FFDFC0')
-        root.style.setProperty('$primary-color', '#341f0a')
-        main.dataset.theme = 'dark'
-    }
-})
+    toggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('light-theme');
+        document.body.classList.toggle('dark-theme');
+    });
+});
